9c66447de92fe75fa5888fc8246f0d05422eadf8
lovelycat <ruysith@gamil.com>
2018-05-28 16:47:48 +0900

    Initial Commit

---
+++ test2018/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test2018
+//
+//  Created by lovelycat on 2018. 5. 28..
+//  Copyright © 2018년 lovelycat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test2018/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test2018
+//
+//  Created by lovelycat on 2018. 5. 28..
+//  Copyright © 2018년 lovelycat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
